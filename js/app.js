ba71fdf15861a7b66e6da832d901b245da5524c2
jessicavaleria <jessica.goncar.97@gmail.com>
2017-11-16 16:30:21 -0300

    header

--- js/app.js
+++ js/app.js
@@ -1,27 +1,6 @@
-var submenu= document.getElementsByClassName('subMenu');
-//llamamos las clases de los items
-
-//iteramos por los elementos
-for (var i = 0; i < submenu.length; i++) {
-	submenu[i].addEventListener('click',showMenu)
-}
-
-function showMenu(){
-	var listmenu=this.getElementsByClassName('itemList')[0];
-	console.log(listmenu);
-	if(listmenu.classList.contains('hide')){
-			listmenu.classList.remove('hide');
-			listmenu.classList.add('show');
-	} else {
-		listmenu.classList.remove('show');
-		listmenu.classList.add('hide');
-	}
-
-
-}
 /*
  * Funcionalidad de tu producto
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-//console.log(data);
+console.log(data);
