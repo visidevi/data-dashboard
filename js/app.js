78eca2d80c92e7fb287471e1a30e1c046a3ca85a
visaka <visidevi@gmail.com>
2017-11-16 16:31:59 -0300

    js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,24 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+var submenu= document.getElementsByClassName('subMenu');
+//llamamos las clases de los items
+
+//iteramos por los elementos
+for (var i = 0; i < submenu.length; i++) {
+	submenu[i].addEventListener('click',showMenu)
+}
+
+function showMenu(){
+	var listmenu=this.getElementsByClassName('itemList')[0];
+	console.log(listmenu);
+	if(listmenu.classList.contains('hide')){
+			listmenu.classList.remove('hide');
+			listmenu.classList.add('show');
+	} else {
+		listmenu.classList.remove('show');
+		listmenu.classList.add('hide');
+	}
+
+
+}
