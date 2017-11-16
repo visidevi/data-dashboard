938c03de6db2f8bcf1ab82c43021b3b044c492d8
visaka <visidevi@gmail.com>
2017-11-16 18:24:08 -0300

    menu hamburgesa

--- js/app.js
+++ js/app.js
@@ -23,5 +23,49 @@ function showMenu(){
 		listmenu.classList.add('hide');
 	}
 
+};
+ function hamburger(){
+ 	var menuhamburger=document.createElement('div');
+ 	menuhamburger.setAttribute('class', 'hamburger');
 
-}
+ 	var ul=document.createElement('ul');
+ 	var user=document.createElement('li');
+ 	var addstudent=document.createElement('li');
+ 	var removestudent=document.createElement('li');
+ 	var addsprint=document.createElement('li');
+
+
+ 	 var buttonclose = document.createElement('i')
+	 buttonclose.setAttribute('class','fa fa-times-circle');
+	 buttonclose.setAttribute('aria-hidden',"true");
+
+	 menuhamburger.appendChild(buttonclose);
+
+	var img = document.createElement
+	
+
+ 	ul.appendChild(user);
+ 	ul.appendChild(addstudent);
+ 	ul.appendChild(removestudent);
+ 	ul.appendChild(addsprint);
+
+ 	user.textContent = 'Valentina Smith';
+ 	addstudent.textContent = 'Add student';
+ 	removestudent.textContent = 'Remove student';
+ 	addsprint.textContent = 'Add Sprint';
+
+console.log(ul);
+	
+	menuhamburger.appendChild(ul);
+	
+	document.body.appendChild(menuhamburger);
+
+	buttonclose.addEventListener('click',function(){
+		document.body.removeChild(menuhamburger);
+		console.log('hola')
+	});
+
+	
+
+ };
+console.log('hola')
