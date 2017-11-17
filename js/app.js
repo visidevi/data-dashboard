8e838ace36d05da49c469bb816f4c42ae41c7ed1
jessicavaleria <jessica.goncar.97@gmail.com>
2017-11-16 22:49:07 -0300

    modificando

--- js/app.js
+++ js/app.js
@@ -7,22 +7,9 @@ console.log(data);
 var menu= document.getElementById('subMenu');
 var list = document.getElementById('hide');
 //llamamos las clases de los items
-
-//iteramos por los elementos
-for (var i = 0; i < submenu.length; i++) {
-	submenu[i].addEventListener('click',showMenu)
-}
-
-function showMenu(){
-	var listmenu=this.getElementsByClassName('itemList')[0];
-	console.log(listmenu);
-	if(listmenu.classList.contains('hide')){
-			listmenu.classList.remove('hide');
-			listmenu.classList.add('show');
-	} else {
-		listmenu.classList.remove('show');
-		listmenu.classList.add('hide');
-	}
+menu.addEventListener('click', function() {
+	list.toggle('show')
+});
 
 };
  function hamburger(){
